5ec28cccfcee9fddf5e8d486cc4476e49ea1e3c2
salmaabzou <salmaabzou@gmail.com>
2023-09-11 22:50:56 +0100

    facto

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,14 @@
-function factorial (a) {
-  if ((Number.isNaN(a)) || (a === 1)) {
+#!/usr/bin/node
+
+function fact (num) {
+  if (isNaN(num) || num === 0) {
     return 1;
   }
-  return factorial(a - 1) * a;
+  return num * fact(num - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+const num = +process.argv[2];
+
+const res = fact(num);
+
+console.log(res);
